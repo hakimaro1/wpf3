288a66c81074e164ad01b44bb128a880762ecf15
hakimaro1 <sanya.kashirin1969@bk.ru>
2026-02-09 09:32:09 +0300

    Применение маршрутизированных событий

---
+++ WpfHello/WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace WpfHello
+{
+    public partial class MainWindow : Window
+    {
+        bool isDataDirty = false;
+        string nameFile = "username.txt";  
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbl.Content = "Добрый день!";
+            setBut.IsEnabled = false;
+            retBut.IsEnabled = false;
+        }
+
+       
+        private void SetBut()
+        {
+            using (StreamWriter sw = new StreamWriter(nameFile))
+            {
+                sw.WriteLine(setText.Text);
+            }
+            retBut.IsEnabled = true;
+            isDataDirty = false;
+        }
+
+        
+        private void RetBut()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(nameFile))
+                {
+                    retLabel.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            try
+            {
+                switch (feSource.Name)
+                {
+                    case "setBut":
+                        SetBut();
+                        break;
+                    case "retBut":
+                        RetBut();
+                        break;
+                }
+                e.Handled = true;  
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            setBut.IsEnabled = true;
+            isDataDirty = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения?";
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+            isDataDirty = false;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public MyWindow myWin { get; set; }
+
+        private void New_Win_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (myWin == null)
+                myWin = new MyWindow();
+            myWin.Owner = this;
+            var location = New_Win.PointToScreen(new Point(0, 0));
+            myWin.Left = location.X + New_Win.Width;
+            myWin.Top = location.Y;
+            myWin.Show();
+        }
+    }
+}
---
+++ WpfHello/WpfHello/MyWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Логика взаимодействия для MyWindow.xaml
+    /// </summary>
+    public partial class MyWindow : Window
+    {
+        MainWindow wnd1 = null;
+        private bool _close;
+        public MyWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        public new void Close()
+        {
+            _close = true;
+            base.Close();
+        }
+        private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_close) return;
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            wnd1 = Owner as MainWindow;
+            if (wnd1 != null)
+            {
+                wnd1.myWin = null;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // a. Инициализируем ссылку wnd1 с помощью свойства Owner
+            wnd1 = Owner as MainWindow;
+
+            // b. Присваиваем свойству Text текстового блока txtBlock главного окна
+            // текст текстового поля второго окна
+            if (wnd1 != null)
+            {
+                // Здесь нужна проверка, что txtBlock существует в MainWindow
+                // Добавим текстовый блок в MainWindow если его нет
+                wnd1.txtBlock.Text = textBox.Text;
+            }
+            wnd1.txtBlock.Text = textBox.Text;
+            PrintLogFile();
+            Close();
+        }
+        private void PrintLogFile()
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("log.txt", true))
+            {
+                writer.WriteLine("Внесено {0}: {1} ", textBox.Text,
+                    DateTime.Now.ToShortDateString() + ", время: " +
+                    DateTime.Now.ToLongTimeString());
+                writer.Flush();
+            }
+        }
+    }
+}
